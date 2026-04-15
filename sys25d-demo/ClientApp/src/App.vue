df695e94f2a3a89cddc2226a1c25271c9c92780f
Jenni <jenni@posh.se>
2026-04-15 11:07:30 +0300

    create frontend login form

--- sys25d-demo/ClientApp/src/App.vue
+++ sys25d-demo/ClientApp/src/App.vue
@@ -1,85 +1,61 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
+import { ref } from "vue";
+
+const username = ref('');
+const password = ref('');
+
+function loginUser(e: MouseEvent) {
+  e.preventDefault();
+
+  console.log('klickade på knappen');
+  console.log(username.value);
+  console.log(password.value);
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>Hello World</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <form>
+    <label>
+      <span>Användarnamn:</span>
+      <input type="text" v-model="username">
+    </label>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <label>
+      <span>Lösenord:</span>
+      <input type="password" v-model="password">
+    </label>
+
+    <button @click="loginUser">Login</button>
+  </form>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+@reference "./styles/style.css";
+
+h1 {
+  font-size: 3rem;
+  font-weight: bold;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+label input {
+  @apply border px-2;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+button {
+  @apply bg-orange-400 text-white font-bold px-4 py-2 rounded;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  /*
+  background-color: orange;
+  color: white;
+  font-weight: bold;
+  padding-inline: 8px;
+  padding-block: 4px;
+  border-radius: 8px;
+   */
 }
 </style>
